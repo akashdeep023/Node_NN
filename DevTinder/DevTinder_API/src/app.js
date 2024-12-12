fde59de6f3c9b1b413f35bcb0c1f5738e44ae849
akashdeep023 <ad3500476@gmail.com>
2024-12-12 16:51:55 +0530

    Create POST /signup API handler S2E6

--- DevTinder/DevTinder_API/src/app.js
+++ DevTinder/DevTinder_API/src/app.js
@@ -1,61 +1,37 @@
 const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
-// Admin Middleware
-app.use("/admin", (req, res, next) => {
-	const token = "xyz";
-	const isAdminAuth = token === "xyz";
-	if (!isAdminAuth) {
-		res.status(401).send("unauthorized");
-	} else {
-		next();
+// signup
+app.post("/signup", async (req, res) => {
+	// create a new user (instance) of the User model
+	const user = new User({
+		firstName: "Akash",
+		lastName: "Kumar",
+		emailId: "ak@gmail.com",
+		password: "Akash@123",
+	});
+	try {
+		// save the user to the database
+		await user.save();
+		// return a success response
+		res.send("User created successfully");
+	} catch (err) {
+		// return an error response if the save operation fails
+		res.status(500).send(
+			"Server error while creating user : " + err.message
+		);
 	}
 });
 
-// User Middleware
-const userAuth = (req, res, next) => {
-	const token = "xyz";
-	const isUserAuth = token === "xyz";
-	if (!isUserAuth) {
-		res.status(401).send("unauthorized");
-	} else {
-		next();
-	}
-};
-
-app.get("/admin/allData", (req, res) => {
-	res.send("All data sent!");
-});
-app.get("/admin/delete", (req, res) => {
-	res.send("Deleted!");
-});
-
-// userAuth - middleware
-app.get("/user/data", userAuth, (req, res) => {
-	res.send("Data sent!");
-});
-app.get("/user/login", (req, res) => {
-	res.send("Login!");
-});
-
-// Parameters in the request headers
-// 2 parameters -> request, response
-// 3 parameters -> request, response, next
-// 4 parameters -> error, request, response, next
-
-app.get("/dashboard", (req, res) => {
-	console.log("/dashboard route");
-	throw new Error("dashboard route not available");
-});
-
-// Error handling
-// using middleware or try catch block
-app.use("/", (err, req, res, next) => {
-	if (err) {
-		res.status(500).send("Something went wrong");
-	}
-});
-
-app.listen(7777, () => {
-	console.log("Server is successfully listening on 7777 port...");
-});
+connectDB()
+	.then(() => {
+		console.log("Database connection established...");
+		app.listen(7777, () => {
+			console.log("Server is successfully listening on 7777 port...");
+		});
+	})
+	.catch((err) => {
+		console.error(err);
+	});
