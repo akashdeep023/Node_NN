7285eb01a7ab2a86b43819d5a7767cb980e47a29
akashdeep023 <ad3500476@gmail.com>
2024-12-12 19:51:34 +0530

    Create GET API handlers S2E7

--- DevTinder/DevTinder_API/src/app.js
+++ DevTinder/DevTinder_API/src/app.js
@@ -24,6 +24,56 @@ app.post("/signup", async (req, res) => {
 	}
 });
 
+// get filter users in the database
+app.get("/user", async (req, res) => {
+	try {
+		// single user found by emailId - findOne({ emailId: email})
+		const user = await User.findOne({ emailId: req.body.emailId });
+		if (!user) {
+			res.status(404).send("User not found.");
+		} else {
+			res.send(user);
+		}
+
+		// array of users found by emailId - find({ emailId: email})
+		// const users = await User.find({ emailId: req.body.emailId });
+		// if (users.length > 0) {
+		// 	res.send(users);
+		// } else {
+		// 	res.status(404).send("No users found.");
+		// }
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send("Server error while retrieving users.");
+	}
+});
+
+// get user in the database by id - user object
+app.get("/user/:id", async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) {
+			res.status(404).send("No users found.");
+		} else {
+			res.send(user);
+		}
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send("Server error while retrieving users.");
+	}
+});
+
+// get all users in the database - array of users
+app.get("/feed", async (req, res) => {
+	try {
+		const users = await User.find({});
+		res.send(users);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send("Server error while retrieving users.");
+	}
+});
+
 connectDB()
 	.then(() => {
 		console.log("Database connection established...");
