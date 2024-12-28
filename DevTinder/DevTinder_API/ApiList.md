6c2eec3ebb80836f93de6bf77d8f61b580027948
akashdeep023 <ad3500476@gmail.com>
2024-12-29 01:15:53 +0530

    Create GET /user/feed API & Pagination S2E14

--- DevTinder/DevTinder_API/ApiList.md
+++ DevTinder/DevTinder_API/ApiList.md
@@ -17,10 +17,20 @@
 -   POST /request/send/:status/:userId
 -   POST /request/review/:status/:requestId
 
-Status: `ignored`, `interested`, `accepeted`, `rejected`
+Status: `ignored`, `interested`, `accepted`, `rejected`
 
 ## userRouter
 
 -   GET /user/requests/recieved
 -   GET /user/connections
 -   GET /user/feed - Gets you the profiles of other users on platform
+
+## Pagination
+
+-   `/user/feed?page=1&limit=10` => `1 to 10` => `skip(0)` & `limit(10)`
+-   `/user/feed?page=2&limit=10` => `11 to 20` => `skip(10)` & `limit(10)`
+-   `/user/feed?page=3&limit=10` => `21 to 30` => `skip(20)` & `limit(10)`
+-   `/user/feed?page=4&limit=10` => `31 to 40` => `skip(30)` & `limit(10)`
+
+-   **`page` = 1 & `limit` = 10**
+-   **`skip` = `(page-1) * limit`**
--- DevTinder/DevTinder_API/src/routes/user.js
+++ DevTinder/DevTinder_API/src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-const user = require("../models/user");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 // Get the all pending connection requests for the loggedIn user
@@ -49,6 +49,53 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 		res.json({
 			message: "Data fetched successfully",
 			data,
+		});
+	} catch (err) {
+		res.status(400).send("ERROR: " + err.message);
+	}
+});
+
+// Get the all users cards except
+// 1. His own card
+// 2. His connection -> accepted
+// 3. Ignored users -> ignored
+// 4. Already send tha connection request -> interested
+// 5. Rejected users -> rejected
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+	try {
+		// Pagination
+		const page = parseInt(req.query.page) || 1;
+		let limit = parseInt(req.query.limit) || 10;
+		limit = limit > 50 ? 50 : limit;
+		const skip = (page - 1) * limit;
+
+		// Find all user connection
+		const connectionRequests = await ConnectionRequest.find({
+			$or: [{ fromUserId: req.user._id }, { toUserId: req.user._id }],
+		}).select("fromUserId toUserId");
+
+		// Create a new set of users to store userId (own, connection, ignored, request, rejected)
+		let hideUserFromFeed = new Set();
+		connectionRequests.forEach((c) => {
+			hideUserFromFeed.add(c.fromUserId.toString());
+			hideUserFromFeed.add(c.toUserId.toString());
+		});
+
+		const USER_SAFE_DATA =
+			"firstName lastName photoUrl age gender about skills";
+		const users = await User.find({
+			$and: [
+				{ _id: { $nin: Array.from(hideUserFromFeed) } }, // not his connection
+				{ _id: { $ne: req.user._id } }, // not his own card
+			],
+		})
+			.select(USER_SAFE_DATA)
+			.skip(skip)
+			.limit(limit);
+
+		res.json({
+			message: "Data fetched successfully",
+			data: users,
 		});
 	} catch (err) {
 		res.status(400).send("ERROR: " + err.message);
